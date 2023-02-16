7c53165e95d0f62db68da00455cad7a92d7bee85
ishvinder9528 <ishvinder133@gmail.com>
2023-02-17 00:01:33 +0530

    add codemon

---
+++ backend/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.7.1"
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI, ()=>{console.log("Connecte to Mongo Successfully")})
+}
+module.exports = connectToMongo
